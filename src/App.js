b65fad94dbd0d3163931697d40fa4e9bdead1267
lishunyang <lishunyang@mokahr.com>
2021-08-09 00:28:26 +0800

    🎉 initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
+
+  const initialValue = '<p>Name: <variable>username</variable></p>';
+  const [name, setName] = useState('username');
+  const onVariableChange = (e) => {
+    const value = e.target.value;
+    setName(value);
+    tinymce.walk(editorRef.current.getBody(), function(n) {
+      if (n.nodeType === 3 && n.parentNode && n.parentNode.tagName === 'VARIABLE') {
+        n.nodeValue = value;
+      }
+    }, 'childNodes');
+  }
+  const onEditorChange = (newValue, editor) => {
+    /*global tinymce*/
+    tinymce.walk(editor.getBody(), function(n) {
+      if (n.nodeType === 3 && n.parentNode && n.parentNode.tagName === 'VARIABLE') {
+        setName(n.nodeValue);
+      }
+    }, 'childNodes');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="left">
+        <div>
+          <label htmlFor="username">User name</label>
+          <input id="username" value={name} onChange={onVariableChange}/>
+        </div>
+        <button onClick={log}>Log editor content</button>
+      </div>
+      <div className="right">
+        <Editor
+          tinymceScriptSrc="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js"
+          onInit={(evt, editor) => editorRef.current = editor}
+          initialValue={initialValue}
+          init={{
+            height: 500,
+            menubar: false,
+            plugins: [
+              'advlist autolink lists link image charmap print preview anchor',
+              'searchreplace visualblocks code fullscreen',
+              'insertdatetime media table paste code help wordcount'
+            ],
+            toolbar: 'undo redo | formatselect | ' +
+            'bold italic backcolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }' +
+              'variable { padding: 4px; background-color: #e4e4e4 }',
+            branding: false.valueOf,
+            extended_valid_elements: 'variable',
+            custom_elements: '~variable',
+          }}
+          onEditorChange={onEditorChange}
+        />
+      </div>
     </div>
   );
 }
-
-export default App;
